3efb89b1a713582388febb3b285b85dc4e10f3a0
Lohit <lohit.pereira09@stanislausbandra.in>
2020-01-07 01:35:10 +0530

    lp | temporary graphs rendering

--- src/components/pages/Analytics.js
+++ src/components/pages/Analytics.js
@@ -1,17 +1,437 @@
 import React,{ Component } from 'react';
+import Highcharts from 'highcharts';
+import HighchartsReact from 'highcharts-react-official';
 //import PropTypes from 'prop-types';
 
 class Analytics extends Component{
 
+  
+
   render(){
     return (
       <React.Fragment>
-        <h1>Analytics</h1>
-        <p>This is the aAn alytics Module</p>
+        <div class="row col-md-12">
+          <div class="col-md-6">
+            <HighchartsReact highcharts={Highcharts} options={baroptions} />
+          </div>
+          <div class="col-md-6">
+            <HighchartsReact highcharts={Highcharts} options={splineoptions} />
+          </div>
+          <div class="col-md-12">
+            <HighchartsReact highcharts={Highcharts} options={modeOfInteraction} />
+          </div>
+        </div>
       </React.Fragment>
     )
   }
 }
 
+const baroptions = { 
+  chart: {
+      type: 'column'
+  },
+  title: {
+      text: 'Browser market shares. January, 2018'
+  },
+  subtitle: {
+      text: 'Click the columns to view versions. Source: <a href="http://statcounter.com" target="_blank">statcounter.com</a>'
+  },
+  accessibility: {
+      announceNewData: {
+          enabled: true
+      }
+  },
+  xAxis: {
+      type: 'category'
+  },
+  yAxis: {
+      title: {
+          text: 'Total percent market share'
+      }
+
+  },
+  legend: {
+      enabled: false
+  },
+  plotOptions: {
+      series: {
+          borderWidth: 0,
+          dataLabels: {
+              enabled: true,
+              format: '{point.y:.1f}%'
+          }
+      }
+  },
+
+  tooltip: {
+      headerFormat: '<span style="font-size:11px">{series.name}</span><br>',
+      pointFormat: '<span style="color:{point.color}">{point.name}</span>: <b>{point.y:.2f}%</b> of total<br/>'
+  },
+
+  series: [
+      {
+          name: "Browsers",
+          colorByPoint: true,
+          data: [
+              {
+                  name: "Chrome",
+                  y: 62.74,
+                  drilldown: "Chrome"
+              },
+              {
+                  name: "Firefox",
+                  y: 10.57,
+                  drilldown: "Firefox"
+              },
+              {
+                  name: "Internet Explorer",
+                  y: 7.23,
+                  drilldown: "Internet Explorer"
+              },
+              {
+                  name: "Safari",
+                  y: 5.58,
+                  drilldown: "Safari"
+              },
+              {
+                  name: "Edge",
+                  y: 4.02,
+                  drilldown: "Edge"
+              },
+              {
+                  name: "Opera",
+                  y: 1.92,
+                  drilldown: "Opera"
+              },
+              {
+                  name: "Other",
+                  y: 7.62,
+                  drilldown: null
+              }
+          ]
+      }
+  ],
+  drilldown: {
+      series: [
+          {
+              name: "Chrome",
+              id: "Chrome",
+              data: [
+                  [
+                      "v65.0",
+                      0.1
+                  ],
+                  [
+                      "v64.0",
+                      1.3
+                  ],
+                  [
+                      "v63.0",
+                      53.02
+                  ],
+                  [
+                      "v62.0",
+                      1.4
+                  ],
+                  [
+                      "v61.0",
+                      0.88
+                  ],
+                  [
+                      "v60.0",
+                      0.56
+                  ],
+                  [
+                      "v59.0",
+                      0.45
+                  ],
+                  [
+                      "v58.0",
+                      0.49
+                  ],
+                  [
+                      "v57.0",
+                      0.32
+                  ],
+                  [
+                      "v56.0",
+                      0.29
+                  ],
+                  [
+                      "v55.0",
+                      0.79
+                  ],
+                  [
+                      "v54.0",
+                      0.18
+                  ],
+                  [
+                      "v51.0",
+                      0.13
+                  ],
+                  [
+                      "v49.0",
+                      2.16
+                  ],
+                  [
+                      "v48.0",
+                      0.13
+                  ],
+                  [
+                      "v47.0",
+                      0.11
+                  ],
+                  [
+                      "v43.0",
+                      0.17
+                  ],
+                  [
+                      "v29.0",
+                      0.26
+                  ]
+              ]
+          },
+          {
+              name: "Firefox",
+              id: "Firefox",
+              data: [
+                  [
+                      "v58.0",
+                      1.02
+                  ],
+                  [
+                      "v57.0",
+                      7.36
+                  ],
+                  [
+                      "v56.0",
+                      0.35
+                  ],
+                  [
+                      "v55.0",
+                      0.11
+                  ],
+                  [
+                      "v54.0",
+                      0.1
+                  ],
+                  [
+                      "v52.0",
+                      0.95
+                  ],
+                  [
+                      "v51.0",
+                      0.15
+                  ],
+                  [
+                      "v50.0",
+                      0.1
+                  ],
+                  [
+                      "v48.0",
+                      0.31
+                  ],
+                  [
+                      "v47.0",
+                      0.12
+                  ]
+              ]
+          },
+          {
+              name: "Internet Explorer",
+              id: "Internet Explorer",
+              data: [
+                  [
+                      "v11.0",
+                      6.2
+                  ],
+                  [
+                      "v10.0",
+                      0.29
+                  ],
+                  [
+                      "v9.0",
+                      0.27
+                  ],
+                  [
+                      "v8.0",
+                      0.47
+                  ]
+              ]
+          },
+          {
+              name: "Safari",
+              id: "Safari",
+              data: [
+                  [
+                      "v11.0",
+                      3.39
+                  ],
+                  [
+                      "v10.1",
+                      0.96
+                  ],
+                  [
+                      "v10.0",
+                      0.36
+                  ],
+                  [
+                      "v9.1",
+                      0.54
+                  ],
+                  [
+                      "v9.0",
+                      0.13
+                  ],
+                  [
+                      "v5.1",
+                      0.2
+                  ]
+              ]
+          },
+          {
+              name: "Edge",
+              id: "Edge",
+              data: [
+                  [
+                      "v16",
+                      2.6
+                  ],
+                  [
+                      "v15",
+                      0.92
+                  ],
+                  [
+                      "v14",
+                      0.4
+                  ],
+                  [
+                      "v13",
+                      0.1
+                  ]
+              ]
+          },
+          {
+              name: "Opera",
+              id: "Opera",
+              data: [
+                  [
+                      "v50.0",
+                      0.96
+                  ],
+                  [
+                      "v49.0",
+                      0.82
+                  ],
+                  [
+                      "v12.1",
+                      0.14
+                  ]
+              ]
+          }
+      ]
+  }
+};
+
+const splineoptions = {
+  chart: {
+      type: 'line'
+  },
+  title: {
+      text: 'Monthly Average Temperature'
+  },
+  subtitle: {
+      text: 'Source: WorldClimate.com'
+  },
+  xAxis: {
+      categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+  },
+  yAxis: {
+      title: {
+          text: 'Temperature (°C)'
+      }
+  },
+  plotOptions: {
+      line: {
+          dataLabels: {
+              enabled: true
+          },
+          enableMouseTracking: false
+      }
+  },
+  series: [{
+      name: 'Tokyo',
+      data: [7.0, 6.9, 9.5, 14.5, 18.4, 21.5, 25.2, 26.5, 23.3, 18.3, 13.9, 9.6]
+  }, {
+      name: 'London',
+      data: [3.9, 4.2, 5.7, 8.5, 11.9, 15.2, 17.0, 16.6, 14.2, 10.3, 6.6, 4.8]
+  }]
+};
+
+const modeOfInteraction = {
+  title: {
+      text: 'Combination chart'
+  },
+  xAxis: {
+      categories: ['Apples', 'Oranges', 'Pears', 'Bananas', 'Plums']
+  },
+  labels: {
+      items: [{
+          html: 'Total fruit consumption',
+          style: {
+              left: '50px',
+              top: '18px',
+              color: ( // theme
+                  Highcharts.defaultOptions.title.style &&
+                  Highcharts.defaultOptions.title.style.color
+              ) || 'black'
+          }
+      }]
+  },
+  series: [{
+      type: 'column',
+      name: 'Jane',
+      data: [3, 2, 1, 3, 4]
+  }, {
+      type: 'column',
+      name: 'John',
+      data: [2, 3, 5, 7, 6]
+  }, {
+      type: 'column',
+      name: 'Joe',
+      data: [4, 3, 3, 9, 0]
+  }, {
+      type: 'spline',
+      name: 'Average',
+      data: [3, 2.67, 3, 6.33, 3.33],
+      marker: {
+          lineWidth: 2,
+          lineColor: Highcharts.getOptions().colors[3],
+          fillColor: 'white'
+      }
+  }, {
+      type: 'pie',
+      name: 'Total consumption',
+      data: [{
+          name: 'Jane',
+          y: 13,
+          color: Highcharts.getOptions().colors[0] // Jane's color
+      }, {
+          name: 'John',
+          y: 23,
+          color: Highcharts.getOptions().colors[1] // John's color
+      }, {
+          name: 'Joe',
+          y: 19,
+          color: Highcharts.getOptions().colors[2] // Joe's color
+      }],
+      center: [100, 80],
+      size: 100,
+      showInLegend: false,
+      dataLabels: {
+          enabled: false
+      }
+  }]
+};
 
 export default Analytics;
