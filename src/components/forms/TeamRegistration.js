b86bd0593511da688252e7bb45810b200806f4c1
Lohit <lohit.pereira09@stanislausbandra.in>
2020-04-06 21:18:01 +0530

    form validations to be imploemented

--- src/components/forms/TeamRegistration.js
+++ src/components/forms/TeamRegistration.js
@@ -9,30 +9,40 @@ class TeamRegistration extends Component{
         super(props);
         this.state={
             RegistrationStep:1,
-            firstName:"",
-            lastName:"",
-            contact:"",
-            registrationType:0,
-            profileImage:"",
-            email:"",
-            password:"",
-            teamName:"",
-            teamPlayers:[]
+            formFields:{
+                firstName:"",
+                lastName:"",
+                contact:"",
+                registrationType:null,
+                profileImage:"",
+                email:"",
+                password:"",
+                teamName:"",
+                teamPlayers:[]
+            }
         };
     }
 
     handleChange =(event)=>{
-        this.setState({[event.target.name] : event.target.value});
+        let newState = Object.assign({}, this.state);
+        newState.formFields[event.target.name] = event.target.value;
+        this.setState(newState);
+        //this.setState({formFields[event.target.name] : event.target.value});
+    }
+    
+    submitTeamRegistration = (event) =>{
+        event.preventDefault();
+        console.log(this.state.formFields);
     }
     
     render(){
         return(
-            <form onSubmit={this.handleSubmit}>
+            <form onSubmit={this.handleSubmit} >
                 <Col lg={12} md={12} sm={12} xs={12}>
                     <Row lg={2} md={2} sm={2} xs={2}>
                         <Col lg={6} md={6} sm={6} xs={6} >
                             <label htmlFor="inputEmail4">First Name</label>
-                            <input type="text" name="name"  onChange={this.handleChange} className="form-control" placeholder="Cristiano" />
+                            <input type="text" name="firstName"  onChange={this.handleChange} className="form-control" placeholder="Cristiano" />
                         </Col>
                         <Col lg={6} md={6} sm={6} xs={6} >
                             <label htmlFor="inputEmail4">Last Name</label>
@@ -46,40 +56,31 @@ class TeamRegistration extends Component{
                         </Col>
                         <Col lg={6} md={6} sm={12} xs={12} className="form-group">
                             <label htmlFor="inputEmail4">Email</label>
-                            <input type="email" name="email" onChange={this.handleChange} className="form-control" id="inputEmail4" placeholder="Email" />
+                            <input type="email" name="email" onChange={this.handleChange} className="form-control" id="inputEmail4" placeholder="cristiano@cr7.com" />
                         </Col>
                         <Col lg={6} md={6} sm={12} xs={12} className="form-group">
                             <label htmlFor="inputPassword4">Password</label>
-                            <input type="password" name="password" onChange={this.handleChange} className="form-control" id="inputPassword4" placeholder="Password" />
+                            <input type="password" name="password" onChange={this.handleChange} className="form-control" id="inputPassword4" placeholder="cr7@portugal85" />
                         </Col>
                         <Col lg={6} md={6} sm={12} xs={12} className="form-group">
                             <label htmlFor="inputPassword4">Registration Type</label>
-                            <select className="browser-default custom-select" name="registrationType" as="select" custom required>
-                                <option value="0" disabled selected>Manager Or Player</option>
+                            <select defaultValue={'DEFAULT'}  onChange={this.handleChange} className="browser-default custom-select" name="registrationType" as="select" required>
+                                <option value="DEFAULT" disabled >Manager Or Player</option>
                                 <option value="0">Manager</option>
                                 <option value="1">Player</option>
                             </select>
                         </Col>
-                        <Col lg={6} md={6} sm={12} xs={12} className="form-group custom-file" style={{display:"none"}}>
-                            <label htmlFor="inputPassword4">Profile Image</label>
-                            <input
-                                type="file"
-                                name="profileImage"
-                                className=""
-                                id="inputGroupFile01"
-                                custom
-                            />
-                            {/* <file 
-                                id="custom-file-translate-scss"
-                                label="Custom file input"
-                                lang="en"
-                                custom
-                            /> */}
+                        <Col lg={12} md={12} sm={12} xs={12} className="form-group" style={{display:"none"}} >
+                            <input type="file" id="inputGroupFile01" aria-describedby="inputGroupFileAddon01" />
+                            <label className="custom-file-label" htmlFor="inputGroupFile01">Account Image</label>
                         </Col>
                     </Row>
+                    <Row>
+                        
+                    </Row>
                 </Col>
-                <Col md={{ span: 12, offset: 3 }}>
-                    <button type="submit" className="btn btn-primary">Submit</button>
+                <Col  lg={12} md={12} sm={12} xs={12}>
+                    <button type="submit" onClick={this.submitTeamRegistration} className="btn btn-success green-submit-button">Sign up</button>
                 </Col>
             </form>
         )
